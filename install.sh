d6597ecdee61fd1ca84ffdfbcf8d2b65529e01ea
Blaine Pace <blainepace@gmail.com>
2012-10-09 01:33:38 -0700

    Uninstall feature
    Wrote a function to clean up symlinks created by install.sh

--- install.sh
+++ install.sh
@@ -20,15 +20,44 @@ function red {
 function symlink {
   while read filename; do
     target=$HOME/.$filename
-    if [ -e $target ]
+    if [ -e $target ] && [ ! -h $target ]
     then
       echo -e "Error: $(red $target) exists "
       echo "Cowardly dying and refusing to break your local config."
       echo "Clear the obstruction and run this script again to finish up."
       exit
+    elif [ -h $target ]
+    then
+      echo -e "Skipping existing symlink $(yellow $target)"
     else
       echo -e "Linking symlink $(green $target)"
       ln -s $CONFIGDIR/$filename $target
     fi
   done < manifest
 }
+
+function uninstall {
+  while read filename; do
+    target=$HOME/.$filename
+    if [ -h $target ]
+    then
+      echo -e "Removing symlink $(green $target)"
+      rm $target
+    else
+      echo -e "Warning: $(yellow $target) exists, skipping over"
+    fi
+  done < manifest
+  echo -e "Uninstall finished. Feel cleaner?"
+}
+
+case $1 in
+  'install')
+    symlink
+    ;;
+  'uninstall')
+    uninstall
+    ;;
+  *)
+    symlink
+    ;;
+esac
