faddf14c0479fe6c1edd87e9c735ad405c6b0094
Blaine Pace <blainepace@gmail.com>
2014-05-07 20:23:21 -0700

    Refactor bootstrap.sh for readability
    Shamelessly stole some code back from mtarbit/dotfiles for bootstrap.sh.
    Handling of commands and switches is much more readable and intelligent now.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,13 @@ TARGET_DIR=$HOME
 
 MANIFEST='manifest'
 
+args=("$@")
 dryrun=false
 
 function green {
   echo "\033[32m$1\033[0m"
 } 
+
 function yellow {
   echo "\033[33m$1\033[0m"
 }
@@ -22,20 +24,22 @@ function red {
 
 # Sanely and safely creates symlinks.
 # Arguments: $1 = source, $2 = target
-function symlink {
+function create_symlink {
   source=$1
   target=$2
-  if [ -e $target ] && [ ! -h $target ]
-  then
-    echo -e "Error: $(red $target) exists "
-    echo "Cowardly dying and refusing to break your local config."
-    echo "Clear the obstruction and run this script again to finish up."
+  if [ -h $target ]; then
+    echo -e $(yellow "Existing symlink: $target")
+  elif [ ! -e $source ]; then
+    echo -e $(red "Error - source does not exist: $source")
+    echo -e "Correct path or remove from $MANIFEST to continue."
     exit
-  elif [ -h $target ]
-  then
-    echo -e "Skipping existing symlink $(yellow $target)"
+  elif [ -e $target ]; then
+    echo -e $(red "Error - regular file at target location $target exists")
+    echo -e $(red "Remove the obstruction and re-run this script to continue.")
+    exit
   else
-    echo -e "Linking symlink $(green $target)"
+    echo -e $(green "Creating symlink: $target")
+
     if ! $dryrun; then
       ln -s $source $target
     fi
@@ -46,55 +50,87 @@ function symlink {
 # Arguments: $1 = target
 function rm_symlink {
   target=$1
-  if [ -h $target ]
-  then
-    echo -e "Removing symlink $(green $target)"
-    rm $target
-  elif [ -e $target ]
-  then
-    echo -e "Warning: $(yellow $target) is a regular file, skipping over"
+
+  if [ -h $target ]; then
+    echo -e $(green "Removing symlink: $target")
+
+    if ! $dryrun; then
+      rm $target
+    fi
+  elif [ -e $target ]; then
+    echo -e $(red "Warning - Regular file at target location: $target")
   else
-    echo -e "Nothing found at $(green $target), skipping over"
+    echo -e $(yellow "Nothing to do for: $target")
   fi
 }
 
 # 'Installs' the symlinks to the user's home directory.
 function install {
-  echo -e $(green " --- Symlinking Configuration Files --- ")
-  while read file; do
-    symlink $SOURCE_DIR/$file $TARGET_DIR/.$file
+  echo "Installing dotfile symlinks..."
+  while read path; do
+    create_symlink $SOURCE_DIR/$path $TARGET_DIR/.$path
   done < manifest
 
-  echo -e $(green " --- Symlinking Binary Files --- ")
+  echo "Installing binary symlinks..."
   for file in $(ls $BIN_DIR); do
-    symlink $BIN_DIR/$file $TARGET_DIR/bin/$file
+    create_symlink $BIN_DIR/$file $TARGET_DIR/bin/$file
   done
-  echo -e "Install finished."
+  echo "Done"
 }
 
 # Checks for symlinks this script might have created and cleans them up.
 function uninstall {
-  echo -e $(green " --- Removing Configuration Symlinks --- ")
-  while read file; do
-    rm_symlink $TARGET_DIR/.$file
+  echo "Uninstalling dotfile symlinks..."
+  while read path; do
+    rm_symlink $TARGET_DIR/.$path
   done < manifest
 
-  echo -e $(green " --- Removing Binary Symlinks --- ")
+  echo "Uninstalling binary symlinks..."
   for file in $(ls $BIN_DIR); do
     rm_symlink $TARGET_DIR/bin/$file
   done
-  echo -e "Uninstall finished. Feel cleaner?"
+  echo "Done"
 }
 
+function usage {
+  echo "Usage: $script [<command>] [-h|--help] [-d|--dry-run] [-s|--sandbox]"
+  echo "Where <command> is 'install', or 'uninstall'"
+  exit 0
+}
+
+# Parse flags
+for (( i = ${#args[@]} - 1; i >= 0; i-- )); do
+  case ${args[i]} in
+    -d | --dry-run)
+      echo "(DRY RUN: No changes will be made)"
+      dryrun=true
+      unset args[i];;
+    -s | --sandbox)
+      echo "(SANDBOX: Symlinking in sandbox folder)"
+      TARGET_DIR=$SCRIPT_DIR/sandbox
+      if [ ! -d $TARGET_DIR ]; then
+        mkdir $TARGET_DIR
+      fi
+      unset args[i];;
+    -h | --help)
+      usage
+      unset args[i];;
+    -*)
+      echo "Unrecognized option: ${args[i]}"
+      usage
+      unset args[i];;
+  esac
+done
+
+# Check number of arguments
+if [ ${#args[@]} -gt 1 ]; then
+  usage
+fi
+
 # Script start
-case $1 in
-  'install')
-    install
-    ;;
-  'uninstall')
-    uninstall
-    ;;
-  *)
-    install
-    ;;
+case ${args[0]} in
+  ''         ) install;;
+  'install'  ) install;;
+  'uninstall') uninstall;;
+  *          ) usage;;
 esac
