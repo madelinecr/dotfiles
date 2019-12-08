d728e98504b73da76b276b4fdaac65dcf5715dc1
Blaine Pace <blainepace@gmail.com>
2019-12-08 07:51:57 -0800

    Comment out i3 status line code

--- bin/statusline.sh
+++ bin/statusline.sh
@@ -5,13 +5,14 @@ do
   read line
   packages=$(cat ~/.paccount)
 
-  if [ $packages == 0 ]
-  then
-    echo -e ":) | $line" || exit 1
-  elif [ "$packages" -lt "20" ]
-  then
-    echo -e ":o $packages | $line" || exit 1
-  else
-    echo -e "D: $packages | $line" || exit 1
-  fi
+  echo -e "$packages new pkgs | $line" || exit 1
+  #if [ $packages == 0 ]
+  #then
+  #  echo -e ":) | $line" || exit 1
+  #elif [ "$packages" -lt "20" ]
+  #then
+  #  echo -e ":o $packages | $line" || exit 1
+  #else
+  #  echo -e "D: $packages | $line" || exit 1
+  #fi
 done
