f55b2c51c09a6ba1b7592436020d79157b37916a
Blaine Pace <blainepace@gmail.com>
2019-12-12 12:56:41 -0800

    Modify saved tmux session in tmux-monitor4.sh

--- bin/tmux-monitor4.sh
+++ bin/tmux-monitor4.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tmux new-session -s "monitorlong" -d htop
-tmux split-window -v watch -n 0.1 w
 tmux split-window -v watch -n 0.1 iostat
-tmux split-window -v 
+tmux split-window -v watch -n 0.1 w
+tmux split-window -v watch -n 0.1 ls -al /dev/pts*
 tmux attach-session -t "monitorlong"
